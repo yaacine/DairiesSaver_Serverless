556515121db76bb38bc84234756d432ab49cd4b3
yaacine <gy_zidelmal@esi.dz>
2020-02-26 00:58:49 +0100

    updated readme.md

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '46jvxmfez5'
+const apiId = 'gotis1w2rg'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'dev-yaacine.auth0.com',            // Auth0 domain
-  clientId: 'G9nYjlyajarRtIaPdKfm4IsdcbNYT3Gw',          // Auth0 client id
+  clientId: 'TLt3MpxgqH5FKbOH55h6o362gC6zFigV',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
